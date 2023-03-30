e3820bcd269044d7f351400ee89c2eb3548e7e38
Stepanenko1993 <109295820+Stepanenko1993@users.noreply.github.com>
2023-03-30 22:41:42 +0300

    commit 3

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,43 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 
-const list = document.querySelector('.gallery');
+const containerGallery = document.querySelector('.gallery');
+const itemsMarkup = createGallery(galleryItems);
+containerGallery.insertAdjacentHTML('beforeend', itemsMarkup);
 
-(function () {
-    const gallery = galleryItems.map(({ preview, original, description }) => `
- <li class ="gallery__item">
-  <a 
-  class = "gallery__link"
-  href="${original}">
-  <img 
-  class ="gallery__image"
-  src="${preview}" 
-  data-source="${original}
-  alt="${description}"
-  />
+function createGallery(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
   </a>
-</li>`).join('');
-    list.insertAdjacentHTML('beforeend', gallery)
-})()
-
-list.addEventListener('click', onclick);
-
-function onclick(evt) {
+</li>`;
+    })
+    .join('');
+}
+containerGallery.addEventListener('click', onclick);
 
 
-    const instance = basicLightbox.create(`
-    
+
+
+
+
+const instance = basicLightbox.create(
+    `
+  <img width="1280" height="auto" src="">`,
+
+  );
   
-
-`)
-instance.show()
-
-    }
-
-
-   
-    
-
- 
-
-
+  function onclick(evt) {
+    evt.preventDefault();
+    const datasetSource = evt.target.dataset.source;
+    if (!datasetSource) return;
+    instance.element().querySelector('img').src = datasetSource;
+    instance.show();
+  }
+  
 
